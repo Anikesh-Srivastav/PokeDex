d29b77641b6f1846f8313a3c529f97f7ec2175a6
Anikesh Srivastav <131857835+Anikesh-Srivastav@users.noreply.github.com>
2025-06-12 12:00:23 +0530

    Update Pokemon.jsx

--- src/Components/Pokemon.jsx
+++ src/Components/Pokemon.jsx
@@ -8,7 +8,7 @@ const Pokemon = () => {
     const [error, setError] = useState(null);
     const [search, setSearch] = useState("");
 
-    const ApiKey = 'https://pokeapi.co/api/v2/pokemon?limit=124'
+    const ApiKey = "YOUR API KEY HERE"
 
     const CatchingThePokemons = async () => {
         try {
